82d2f170cdaa9051d2174faacd4beb7d928c472d
Joe Rieger <joerieger@quickenloans.com>
2019-07-19 13:47:27 -0400

    Add additional class to identify static vs non-static behavior

--- driver.php
+++ driver.php
@@ -1,22 +1,38 @@
 <?php declare(strict_types=1);
 
+use Jrieger\Xdebug\SampleClass;
 use Jrieger\Xdebug\SampleStaticClass;
 
 require_once 'SampleStaticClass.php';
+require_once 'SampleClass.php';
 
 $resultPrinter = static function (array $results, int $run): void {
     foreach ($results as $file => $result) {
-        if (\strpos($file, 'SampleStaticClass.php') === false) {
+        if (\strpos($file, 'SampleStaticClass.php') === false && \strpos($file, 'SampleClass.php') === false) {
             continue;
         }
-        \printf('Run %2d: Found %d functions in file %s, expected 8', $run, \count($result['functions']), $file);
+
+        $expected = 8;
+        if (\strpos($file, 'SampleClass.php')) {
+            $expected = 4;
+        }
+
+        \printf(
+            'Run %2d: Found %d functions in file %s, expected %d',
+            $run,
+            \count($result['functions']),
+            $file,
+            $expected
+        );
+
+        print PHP_EOL;
     }
 
-    print PHP_EOL . PHP_EOL;
+    print PHP_EOL;
 };
 
 \xdebug_start_code_coverage(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE | XDEBUG_CC_BRANCH_CHECK);
-$object1 = new SampleStaticClass();
+$object1 = new SampleClass();
 assert(SampleStaticClass::getField0() === '0');
 assert(SampleStaticClass::getField1() === '1');
 assert(SampleStaticClass::getField2() === '2');
